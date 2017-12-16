f8550ec1665a62088a3de977605687bf2366502c
crosskyle <kylcross@gmail.com>
2017-12-15 17:04:39 -0800

    Enabled persistence and changed edit wheel size for event collection

--- groupLists/groupLists/AppDelegate.swift
+++ groupLists/groupLists/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
-        
+        Database.database().isPersistenceEnabled = true
         
         return true
     }
